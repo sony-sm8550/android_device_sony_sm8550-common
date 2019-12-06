bcb423af589742d1f7d11f6ea98893d647c0ab51
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    sm8350-common: Use 6GB dalvik-heap config
    We are currently using 2GB dalvik-heap config.
    
    Bug: 145823510
    Test: MPTS
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4

--- common.mk
+++ common.mk
@@ -26,6 +26,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Include common vendor stuff
 $(call inherit-product, vendor/sony/sm8350-common/sm8350-common-vendor.mk)
 
