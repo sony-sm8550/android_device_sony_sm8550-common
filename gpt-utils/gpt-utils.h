d5bf8f94b80457a752c30ca79d8caf6535522bfb
David Ng <dave@codeaurora.org>
2020-09-23 11:32:22 -0700

    sm8350-common: gpt-utils: Add vm-bootsys OTA handling
    Add vm-bootsys to list of partitions needed for A/B update handling.
    
    Change-Id: Ifa1e94df5dd19b1c09088c4b1c435a2aaa84cc62

--- gpt-utils/gpt-utils.h
+++ gpt-utils/gpt-utils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013,2016, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013,2016,2020 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -83,7 +83,8 @@ extern "C" {
 #define PTN_SWAP_LIST                   PTN_XBL, PTN_XBL_CFG, \
             "rpm", "tz", "abl", "hyp", "keymaster", "cmnlib", \
             "cmnlib64", "pmic", "devcfg", "keystore", "mdtp", \
-            "mdtpsecapp", "xfl", "tzxflattest", "tzxfl", "dtbo", "vbmeta"
+            "mdtpsecapp", "xfl", "tzxflattest", "tzxfl", "dtbo", "vbmeta" \
+            "vm-bootsys"
 #define AB_PTN_LIST PTN_SWAP_LIST, "boot", "vendor_boot", "system", "vendor", "modem", "bluetooth", "oem", "rdimage", "dsp"
 #define BOOT_DEV_DIR    "/dev/block/bootdevice/by-name"
 
