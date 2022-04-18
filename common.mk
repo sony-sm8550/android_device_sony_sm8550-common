1e4185e6995585295e07c51ad438eefcc4007c2b
Roopesh Nataraja <quic_roopeshr@quicinc.com>
2022-04-17 23:56:16 -0700

    sm8550-common: Enable compressed snapshots with virtual A/B
    Virtual A/B compression helps to reduce snapshot size on /data.
    Enable compression by inheriting virtual_ab_ota/compression.mk.
    
    Change-Id: I47f241c966285243cdc379ce971c08603ffc575d

--- common.mk
+++ common.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Enable virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
