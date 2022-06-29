dfc937b6f780001a5f6160be4ecca660d7e40c1e
Luofan Chen <clfbbn@gmail.com>
2022-06-29 15:57:47 +0800

    edo-common: Build USB HAL
    Signed-off-by: Luofan Chen <clfbbn@gmail.com>

--- edo.mk
+++ edo.mk
@@ -490,6 +490,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service-qti
+
 # Vendor libstdc++
 PRODUCT_PACKAGES += \
     libstdc++.vendor
