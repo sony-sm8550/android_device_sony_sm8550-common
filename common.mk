4578778aad1193263f02c3c1ac303827147695f7
Neel Chauhan <neel@neelc.org>
2022-07-31 20:19:19 -0700

    sm8550-common: Disable compressed snapshots/VABc
    Virtual AB compression prevents GApps from being installed.
    
    This reverts commits 8d6a934 and f125c68.
    
    Change-Id: I119b8e042e4cdeec27a19ab5dd4f0666cc115ae1

--- common.mk
+++ common.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Enable virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/android_t_baseline.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
@@ -124,8 +124,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
-
-PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := gz
 
 # ANT+
 PRODUCT_PACKAGES += \
