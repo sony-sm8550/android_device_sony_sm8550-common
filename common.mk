f153509eba0cad20e757dc396fa8d19fa1bf8bcc
Aaron Kling <webgeek1234@gmail.com>
2022-08-18 23:17:54 -0500

    sm8350-common: Remove references to a2dp module
    APM would fail to parse the config anyway:
    
      E DevicesFactoryHAL: loadAudioInterface couldn't load audio hw module audio.a2dp (No such file or directory)
      W DevicesFactoryHalHidl: The specified device name is not recognized: "a2dp"
      E AudioFlinger: loadHwModule() error -22 loading module a2dp
      W APM_AudioPolicyManager: could not open HW module a2dp
    
    Change-Id: Iaa1be881cfe8f8474cef0ba46e1b0a62b59e14be

--- common.mk
+++ common.mk
@@ -179,7 +179,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration_a2dp_offload_disabled.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_a2dp_offload_disabled.xml
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
