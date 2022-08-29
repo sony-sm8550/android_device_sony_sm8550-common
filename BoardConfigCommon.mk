b41a9961b8050322ec20edd96b86b0a6d3176336
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    sm8350-common: Switch to legacy sepolicy_vndr
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -224,7 +224,7 @@ VENDOR_SECURITY_PATCH := 2022-06-01
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
