12c93afae55520fb1d9067d8d713dde031e9c99b
Bruno Martins <bgcngm@gmail.com>
2022-09-22 00:40:01 +0100

    sm8350-common: Drop component overrides
    This is no longer needed as of Android 13.
    
    Change-Id: I38435c47c8781678f82014decbbae40dcab0a07b

--- common.mk
+++ common.mk
@@ -257,10 +257,6 @@ PRODUCT_PACKAGES += \
     vendor_modprobe.sh \
     init.sony.rc
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
