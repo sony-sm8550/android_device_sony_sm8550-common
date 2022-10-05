ed463a1bd4b946e9743c703aef2a3e674752efc7
Luofan Chen <clfbbn@gmail.com>
2022-10-05 15:38:23 +0800

    sm8350-common: Commonize props
    Signed-off-by: Luofan Chen <clfbbn@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -200,6 +200,13 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Power
 TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 
+# Properties
+TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
+
 # Recovery
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_USES_RECOVERY_AS_BOOT := true
