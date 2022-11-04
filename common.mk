08f83d95a4eca154157d4ffbe1b14d72b411fd41
Luofan Chen <clfbbn@gmail.com>
2022-11-05 00:58:52 +0800

    sm8350-common: Use boot ctrl version 1.2

--- common.mk
+++ common.mk
@@ -216,9 +216,9 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
