b806bca6bf3e33f0f34c328497fcddce4464ee67
Luofan Chen <clfbbn@gmail.com>
2022-12-30 14:04:56 +0800

    sm8350-common: Fix indentation
    Change-Id: Ic14723cb970a4447acfae5a42da6d2df22434767

--- common.mk
+++ common.mk
@@ -293,7 +293,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-		android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # fastbootd
 PRODUCT_PACKAGES += \
