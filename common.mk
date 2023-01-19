b81f5164e1a8a6b8a3a9984f303e4cb11e83ffc9
Luofan Chen <clfbbn@gmail.com>
2023-01-19 19:22:54 +0800

    sm8350-common: overlay: Convert to RRO packages
    Change-Id: I6f0af19c7e2d0ae0ee67c54ac20399c6bd719443

--- common.mk
+++ common.mk
@@ -38,12 +38,17 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
-    CarrierConfigResCommon
+    CarrierConfigResCommon \
+    SonySagamiFrameworksResCommon \
+    SonySagamiSettingsProviderOverlayCommon \
+    SonySagamiSettingsResCommon \
+    SonySagamiSystemUIResCommon \
+    SonySagamiTelephonyResCommon \
+    WifiResCommon
 
 # Permissions
 PRODUCT_COPY_FILES += \
