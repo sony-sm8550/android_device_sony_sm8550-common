eb8000329672d6daa323fde1c9ae143bcc9ce568
Luofan Chen <clfbbn@gmail.com>
2023-02-25 22:09:45 +0800

    sm8350-common: Clean up unneeded targets
    Change-Id: I0dc9ab9a79e3e32d1a78e932207b78020c86c815

--- common.mk
+++ common.mk
@@ -253,10 +253,6 @@ PRODUCT_PACKAGES += \
     init.sony-platform.rc \
     init.sony.rc
 
-# Configstore
-PRODUCT_PACKAGES += \
-    disable_configstore
-
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
@@ -430,11 +426,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/sony \
     vendor/qcom/opensource/usb/etc
-
-# Sony charger interface
-PRODUCT_PACKAGES += \
-    vendor.semc.hardware.charger@1.0.vendor \
-    vendor.semc.hardware.charger@1.1.vendor
 
 # Sony Display interface
 PRODUCT_PACKAGES += \
