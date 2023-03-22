94b599346677e7c2a549d44d4170a890970e137c
kyasu <kyasu.dev@gmail.com>
2023-03-22 16:36:49 +0900

    sm8350-common: Add pdx214 devices to sim init stuff
    Change-Id: I5b0dec9d1be7076f8cede649c9787fbcffccdf7e

--- rootdir/init.qcom.msim.sh
+++ rootdir/init.qcom.msim.sh
@@ -3,7 +3,7 @@
 model=`grep -am1 '[Mm][Oo][Dd][Ee][Ll]:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*[Mm][Oo][Dd][Ee][Ll]:[ ]*\([A-Za-z0-9-]*\).*$/\1/'` 2> /dev/null
 
 case "$model" in
-    "XQ-BC42" | "XQ-BC52" | "XQ-BC72" )
+    "XQ-BC42" | "XQ-BC52" | "XQ-BC62" | "XQ-BC72" | "XQ-BQ42" | "XQ-BQ52" | "XQ-BQ62" | "XQ-BQ72")
         setprop vendor.radio.multisim.config dsds;;
     * )
         setprop vendor.radio.multisim.config ss;;
