4e143f3bd3416261be63cd967dbe1edb5ebc18d2
Michael W <baddaemon87@gmail.com>
2023-03-29 21:15:23 +0800

    init.qcom.msim.sh: Simplify command that checks model
    Change-Id: I3608cd42335da3b93a7a7b0ecbf82e720ebcd27f

--- rootdir/init.qcom.msim.sh
+++ rootdir/init.qcom.msim.sh
@@ -1,6 +1,6 @@
 #!/vendor/bin/sh
 
-model=`grep -am1 '[Mm][Oo][Dd][Ee][Ll]:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*[Mm][Oo][Dd][Ee][Ll]:[ ]*\([A-Za-z0-9-]*\).*$/\1/'` 2> /dev/null
+model=`grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
 
 case "$model" in
     "XQ-BC42" | "XQ-BC52" | "XQ-BC62" | "XQ-BC72" | "XQ-BQ42" | "XQ-BQ52" | "XQ-BQ62" | "XQ-BQ72")
