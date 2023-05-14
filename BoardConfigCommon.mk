99cc15994d5899950cc00a288e2ad80046cc004d
chandu078 <chandudyavanapelli03@gmail.com>
2023-05-14 16:11:51 +0000

    sm8550-common: Update matrix and manifest to LA.VENDOR.13.2.3.r1-01800-KAILUA.0
    Include the network manifest separately, just like it is now shipped
    in the newest devices.
    
    Change-Id: I491fc3c022f8f6117ef729802a8fd20840911ac4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -183,7 +183,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE :=
+    $(COMMON_PATH)/manifest.xml \
+    $(COMMON_PATH)/network_manifest.xml
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
