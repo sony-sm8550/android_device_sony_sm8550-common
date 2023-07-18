3df58f513dc9528b8b26c8eb9e5c70362fb70a3a
Bruno Martins <bgcngm@gmail.com>
2023-07-18 10:29:25 +0100

    sm8550-common: Ship prebuilt EuiccGoogle and build its companion app
    Change-Id: I389d597093977d2d5883de8bb421edcecf6731fa

--- common.mk
+++ common.mk
@@ -463,6 +463,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    SonyEuicc \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
@@ -476,6 +477,9 @@ PRODUCT_BOOT_JARS += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-euiccgoogle.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-euiccgoogle.xml
 
 # Thermal
 PRODUCT_PACKAGES += \
