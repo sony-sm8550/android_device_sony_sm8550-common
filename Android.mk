c127febae519ae55db2308a62863457bcd17f0a4
lolipuru <banstellazz@gmail.com>
2023-08-20 13:35:21 -0400

    sm8550-common: cleanup some old changes from pdx214
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter pdx214 pdx234,$(TARGET_DEVICE)),)
+ifneq ($(filter pdx234,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "pdx214 pdx234"
+write_headers "pdx234"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
