88551a6fab434405c1c739e08d5cfe2a3750722d
Luofan Chen <clfbbn@gmail.com>
2023-08-20 23:51:57 +0800

    WIP: sm8550-common: Add Xperia 1 V family to msim script
    Change-Id: I9a8ccd66934f19a9944733085a411e6bfe7813c1

--- rootdir/init.qcom.msim.sh
+++ rootdir/init.qcom.msim.sh
@@ -3,7 +3,7 @@
 model=`grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
 
 case "$model" in
-    "XQ-BC42" | "XQ-BC52" | "XQ-BC62" | "XQ-BC72" | "XQ-BQ42" | "XQ-BQ52" | "XQ-BQ62" | "XQ-BQ72")
+    "XQ-BC42" | "XQ-BC52" | "XQ-BC62" | "XQ-BC72" | "XQ-BQ42" | "XQ-BQ52" | "XQ-BQ62" | "XQ-BQ72" | "XQ-DQ72")
         setprop vendor.radio.multisim.config dsds;;
     * )
         setprop vendor.radio.multisim.config ss;;
