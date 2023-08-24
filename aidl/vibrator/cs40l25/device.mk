c40c1401d7d73e53ecfcf56789b08c05e8c5db44
Luofan Chen <clfbbn@gmail.com>
2023-08-24 20:30:40 +0800

    sm8550-common: vibrator: Load calibration data from TA partition
    Change-Id: Icaec9fa562fceaaf9e49b9d42bd935a6dffeb8e5

--- aidl/vibrator/cs40l25/device.mk
+++
@@ -1,6 +0,0 @@
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.cs40l25 \
-
-BOARD_SEPOLICY_DIRS += \
-    hardware/google/pixel-sepolicy/vibrator/common \
-    hardware/google/pixel-sepolicy/vibrator/cs40l25 \
