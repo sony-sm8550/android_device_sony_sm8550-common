bdeff915d412a45358faa50d36282041697eab10
Luofan Chen <clfbbn@gmail.com>
2023-08-27 20:41:45 +0800

    sm8550-common: Build lineage health HAL
    Also wait for the smart_charging_activation node when boot
    
    Change-Id: I1be15297c0684aaa8bb640b459050c134efe9376

--- common.mk
+++ common.mk
@@ -342,7 +342,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
 # Lineage Health
-# PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
 # Media
