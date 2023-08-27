0a65579a1f52efe66be5b7d79338dcab770cfb74
Luofan Chen <clfbbn@gmail.com>
2023-08-27 20:43:00 +0800

    Revert "sm8550-common: build android.hardware.usb.gadget@1.1-service-qti from source"
    This reverts commit 12f69eb9decd618138f79512f188ef6fb3cc831d.

--- common.mk
+++ common.mk
@@ -490,8 +490,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
-    android.hardware.usb.gadget@1.1-service-qti
+    android.hardware.usb@1.3-service-qti
 
 # Vendor libstdc++
 PRODUCT_PACKAGES += \
