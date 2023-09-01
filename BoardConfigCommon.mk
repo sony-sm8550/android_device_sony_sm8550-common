1b276cf217324699ad850af9fd207ffc92507fbc
chandu078 <chandudyavanapelli03@gmail.com>
2023-09-01 08:05:48 +0200

    sm8550-common: Include vendor/oplus/kalama_GKI.config fragment
    Change-Id: Ibcb995e49f518a6e1dd3864cc8b10fe739ce8a7d
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -92,7 +92,8 @@ KERNEL_LTO := none
 TARGET_KERNEL_SOURCE := kernel/sony/sm8550
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
-    vendor/kalama_GKI.config
+    vendor/kalama_GKI.config \
+    vendor/sony/kalama_GKI.config
 
 BOARD_SYSTEM_KERNEL_MODULES := $(strip $(shell cat $(COMMON_PATH)/modules.include.system_dlkm))
 BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.system_dlkm))
