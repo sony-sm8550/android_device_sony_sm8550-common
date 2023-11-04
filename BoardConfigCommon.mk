crackedpotato007/android_device_oneplus_benz

sm8550-common: Include vendor/oplus/kalama_GKI.config fragment
Change-Id: Ibcb995e49f518a6e1dd3864cc8b10fe739ce8a7d
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

BoardConfigCommon.mk
@@ -128,7 +128,8 @@ KERNEL_LTO := none
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8550
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
-    vendor/kalama_GKI.config
+    vendor/kalama_GKI.config \
+    vendor/oplus/kalama_GKI.config
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist

sony-sm8550/android_device_sony_sm8550-common

BoardConfigCommon.mk
@@ -92,7 +92,8 @@ KERNEL_LTO := none
 TARGET_KERNEL_SOURCE := kernel/sony/sm8550
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
-    vendor/kalama_GKI.config
+    vendor/kalama_GKI.config \
+    vendor/sony/kalama_GKI.config
 
 BOARD_SYSTEM_KERNEL_MODULES := $(strip $(shell cat $(COMMON_PATH)/modules.include.system_dlkm))
 BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.system_dlkm))