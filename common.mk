e37a104d0baa9ddb2f117962e3fe8f63ca88854e
Luofan Chen <clfbbn@gmail.com>
2024-01-11 16:24:16 +0800

    [WIP] sm8550-common: LE Audio
    Change-Id: Icb514c7a1d58858ec5b2109804a9813b28451c25

--- common.mk
+++ common.mk
@@ -107,6 +107,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Bluetooth LE Auido offload capabilities setting
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/le_audio_codec_capabilities.xml:$(TARGET_COPY_OUT_VENDOR)/etc/le_audio_codec_capabilities.xml
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti \
