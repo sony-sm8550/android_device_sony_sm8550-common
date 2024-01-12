LineageOS/android_device_oneplus_sm8550-common

sm8550-common: Include vendor/debugfs.config
This disables the following kernel configs:
* CONFIG_DEBUG_FS
* CONFIG_PAGE_OWNER

Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae

BoardConfigCommon.mk
@@ -126,7 +126,8 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/sm8550
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/kalama_GKI.config \
-    vendor/oplus/kalama_GKI.config
+    vendor/oplus/kalama_GKI.config \
+    vendor/debugfs.config
 
 # Kernel modules
 BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.system_dlkm))

sony-sm8550/android_device_sony_sm8550-common

BoardConfigCommon.mk
@@ -91,7 +91,8 @@ TARGET_KERNEL_SOURCE := kernel/sony/sm8550
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/kalama_GKI.config \
-    vendor/sony/kalama_GKI.config
+    vendor/sony/kalama_GKI.config \
+    vendor/debugfs.config
 
 BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.system_dlkm))
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist