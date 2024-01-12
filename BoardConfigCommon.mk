moto-sm8550/android_device_motorola_sm8550-common

sm8550-common: limit dtbs used to generate final dtb image

BoardConfigCommon.mk
@@ -48,6 +48,7 @@ BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
 TARGET_NEEDS_DTBOIMAGE := true
+TARGET_MERGE_DTB_WILDCARD ?= kalama*base
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 4

sony-sm8550/android_device_sony_sm8550-common

Change-Id: I27adf5d4c04507a8e1343f00bc8b190b0366cb66

BoardConfigCommon.mk
@@ -64,6 +64,7 @@ BOARD_RAMDISK_USE_LZ4 := true
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
 TARGET_NEEDS_DTBOIMAGE := true
+TARGET_MERGE_DTBS_WILDCARD ?= kalama*base
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := kalama