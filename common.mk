07b63b2dc2ac46632b2826c07952b51e8ffc4ee4
Luofan Chen <clfbbn@gmail.com>
2024-02-20 22:19:54 +0800

    sm8550-common: Move libchrome to source build
    Change-Id: I0c433bc19b2e6cba366eabd52b3cd91661d4c965

--- common.mk
+++ common.mk
@@ -341,6 +341,7 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
+    libchrome.vendor \
     Tag \
     nqnfcinfo
 
